ced598a9237c6628ab609acca3ecf86c1a38b4e0
deverpham <shahi4@gmail.com>
2020-05-28 11:52:35 +0700

    completed

---
+++ real_test/index.js
@@ -0,0 +1,22 @@
+const { createLogger, registerErrorHandling } = require("../dist");
+const logger = createLogger({
+  transports: ["SENTRY", "CONSOLE"],
+  context: {
+    sentryOpts: {
+      sentry: {
+        dsn:
+          "https://0c04cd22fada47e580ebd9f5c3556d57@o394239.ingest.sentry.io/5256035",
+      },
+      level: "info",
+    },
+  },
+});
+
+registerErrorHandling({
+  logger,
+  isForceQuitWhenErrorOccur: true,
+});
+
+(() => {
+  throw new Error("it should exit in 10 seconds");
+})();
---
+++ src/index.ts
@@ -0,0 +1,144 @@
+import errorParser from "error-stack-parser";
+
+import moment from "moment";
+import winston from "winston";
+import Sentry from "winston-transport-sentry-node";
+
+const colorizer = winston.format.colorize();
+
+const consoleLogFormat = winston.format.combine(
+  winston.format.timestamp(),
+  winston.format.simple(),
+  winston.format.splat(),
+  winston.format.printf((msg) =>
+    colorizer.colorize(
+      msg.level,
+      `${moment
+        .utc(msg.timestamp)
+        .utcOffset(7)
+        .format("DD/MM/YYYY hh:mm:ss")} - ${msg.level}: ${msg.message}`
+    )
+  )
+);
+
+const productionFormat = winston.format.combine(
+  winston.format.timestamp(),
+  winston.format.simple(),
+  winston.format.splat(),
+  winston.format.printf(
+    (msg) =>
+      `${moment
+        .utc(msg.timestamp)
+        .utcOffset(7)
+        .format("DD/MM/YYYY hh:mm:ss")} - ${msg.level}: ${msg.message}`
+  )
+);
+enum TransportType {
+  FILE = "FILE",
+  CONSOLE = "CONSOLE",
+  SENTRY = "SENTRY",
+}
+type GetTransportContext = {
+  sentryOpts: {
+    sentry: {
+      dsn: string;
+    };
+    level: string;
+  };
+};
+const getTransports = (
+  supportedTransports: TransportType[],
+  context: GetTransportContext
+) => {
+  const transports = [];
+  supportedTransports.forEach((transportType) => {
+    switch (transportType) {
+      case TransportType.FILE: {
+        transports.push(
+          new winston.transports.File({
+            level: "error",
+            filename: "logs/errors.log",
+            format: productionFormat,
+          })
+        );
+        break;
+      }
+      case TransportType.CONSOLE: {
+        transports.push(
+          new winston.transports.Console({
+            format: consoleLogFormat,
+          })
+        );
+      }
+      case TransportType.SENTRY: {
+        const sentry = new Sentry(context.sentryOpts);
+        transports.push(sentry);
+      }
+    }
+  });
+  return transports;
+};
+
+const _parserError = (err: Error) => {
+  const errors = errorParser.parse(err);
+  const firstStack = errors[0];
+  return {
+    message: err.message,
+    errorAt: `${firstStack.fileName}:${firstStack.lineNumber}:${firstStack.columnNumber}`,
+  };
+};
+
+export const createLogger = ({
+  transports,
+  context,
+}: {
+  transports: TransportType[];
+  context?: GetTransportContext;
+}) => {
+  return winston.createLogger({
+    transports: getTransports(transports, context),
+  });
+};
+
+// Register function
+export const registerErrorHandling = ({
+  logger,
+  isForceQuitWhenErrorOccur,
+}: {
+  logger: winston.Logger;
+  isForceQuitWhenErrorOccur: boolean;
+}) => {
+  logger.exitOnError = false;
+  process.on("unhandledRejection", async (err: Error) => {
+    const error = _parserError(err);
+    logger.error(
+      "UN_HANDLED_ERROR: message: %s, Error At: %s \n",
+      error.message,
+      error.errorAt
+    );
+    if (isForceQuitWhenErrorOccur) {
+      console.log("error detected ! graceful exit in 7 seconds");
+      setTimeout(() => {
+        process.exit(1);
+      }, 7000);
+    } else {
+      return;
+    }
+  });
+  process.on("uncaughtException", async (err) => {
+    const error = _parserError(err);
+    logger.error(
+      "UN_CAUGHT_ERROR: message: %s, Error At: %s",
+      error.message,
+      error.errorAt
+    );
+    if (isForceQuitWhenErrorOccur) {
+      console.log("error detected ! graceful exit in 7 seconds");
+      setTimeout(() => {
+        process.exit(1);
+      }, 7000);
+    } else {
+      return;
+    }
+  });
+};
